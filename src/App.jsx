d7d36f4f67644426d24d1f3a05d6f6ab6c767e85
Ashma Upreti <ashma.upreti99@gmail.com>
2023-05-25 01:10:31 -0400

    used flex and for loop for lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const names = ["Black", "Brown", "Calico", "White"];
+  let listing_array = [];
+  for (let i = 0; i < names.length; i++) {
+    listing_array[i] = <li>{names[i]}</li>;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header>
+        <ul>
+          <li>
+            <a href="Home">Home</a>
+          </li>
+          <li>
+            <a href="Home">Cat Blog</a>
+          </li>
+          <li>
+            <a href="Home">Our Cats</a>
+          </li>
+          <li>
+            <a href="Home">Contact Us</a>
+          </li>
+        </ul>
+        <button className="button">Get Started</button>
+      </header>
+      <div className="main">
+        <div></div>
+        <div className="list">
+          <li>{listing_array}</li>
+        </div>
+
+        <div className="picture"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <footer>Copywrite@2023</footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
